43c494741fa5b1de73bb51ff8d3648d0b7bc3c9d
julia-nosralla <148015993+julia-nosralla@users.noreply.github.com>
2023-12-06 14:02:10 -0300

    Add files via upload

--- GráficoC.R
+++ GráficoC.R
@@ -1,26 +1,6 @@
 library(tidyverse)
 library(scales)
-
-cores <- c("#e11f25", "#749e5d", "#701d1a", "#e4b784", "#7d4f1b", "#999966", "#006606", "#008091", "#041835", "#666666")
-theme_1 <- function(...) {
-  theme <- ggplot2::theme_bw() +
-    ggplot2::theme(
-      axis.title.y = ggplot2::element_text(colour = "black", size = 12),
-      axis.title.x = ggplot2::element_text(colour = "black", size = 12),
-      axis.text = ggplot2::element_text(colour = "black", size = 9.5),
-      panel.border = ggplot2::element_blank(),
-      axis.line = ggplot2::element_line(colour = "black"),
-      legend.position = "top",
-      ...
-    )
-  return(
-    list(
-      theme,
-      scale_fill_manual(values = cores),
-      scale_colour_manual(values = cores)
-    )
-  )
-}
+library(ggthemes)
 
 continente <- banco %>%
   filter(!is.na(continent)) %>%
@@ -43,7 +23,7 @@ continente <- banco %>%
 
 ggplot(continente) +
   aes(x = fct_reorder(continent, n, .desc=T), y = n, label = label) +
-  geom_bar(stat = "identity", fill = "#e11f25", width = 0.7) +
+  geom_bar(stat = "identity", fill = "#A1D68B", width = 0.7) +
   geom_text(
     position = position_dodge(width = .9),
     vjust = -0.5, hjust = .5,
@@ -51,6 +31,6 @@ ggplot(continente) +
   ) + 
   scale_y_continuous(limits = c(0,1100)) +
   labs(x = "Continente", y = "Número de bilionários") +
-  theme_1()
+  theme_minimal()
 
 ggsave("colunas-uni-freq.pdf", width = 158, height = 93, units = "mm")
